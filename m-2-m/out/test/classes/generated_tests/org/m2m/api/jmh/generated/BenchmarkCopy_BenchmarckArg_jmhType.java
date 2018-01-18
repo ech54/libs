d75d09456e5056aefec94bbc2f3022298153817a
ech54 <ech.charton@gmail.com>
2018-01-18 07:12:09 +0100

    remove local settings

--- m-2-m/out/test/classes/generated_tests/org/m2m/api/jmh/generated/BenchmarkCopy_BenchmarckArg_jmhType.java
+++
@@ -1,4 +0,0 @@
-package org.m2m.api.jmh.generated;
-public class BenchmarkCopy_BenchmarckArg_jmhType extends BenchmarkCopy_BenchmarckArg_jmhType_B3 {
-}
-
--- m-2-m/out/test/classes/generated_tests/org/m2m/api/jmh/generated/BenchmarkCopy_jmhType.java
+++
@@ -1,4 +0,0 @@
-package org.m2m.api.jmh.generated;
-public class BenchmarkCopy_jmhType extends BenchmarkCopy_jmhType_B3 {
-}
-
